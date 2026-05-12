fa10c89b2b0c6ae9c2d3f78e3ee529b192acd991
Anshu8435 <anshugupta15029@gmail.com>
2026-05-13 02:02:54 +0530

    create a Electricity  Bill Generator   & Form Validation Practice in Java Script

--- Projects/To_Do_List/script.js
+++ Projects/To_Do_List/script.js
@@ -42,3 +42,34 @@ function addNewTask() {
 
   document.getElementById("newTask").value = "";
 }
+
+// function addNewTask() {
+//   const tastToAdd = document.getElementById("newTask").value;
+
+//   const LI = document.createElement("li");
+//   LI.classList.add("my-3");
+
+//   const DIV = document.createElement("div");
+//   DIV.classList.add("d-flex", "align-items-center");
+
+//   const SPAN1 = document.createElement("span");
+//   SPAN1.innerText = tastToAdd;
+
+//   const BUTTON = document.createElement("button");
+//   BUTTON.classList.add(
+//     "btn",
+//     "btn-danger",
+//     "ms-4",
+//     "d-flex",
+//     "gap-3",
+//     "align-items-center",
+//     "justify-content-center",
+//   );
+
+//   BUTTON.onclick = () => LI.remove();
+
+//   const I = document.createElement("i");
+//   I.classList.add("bi", "bi-trash");
+
+  
+// }
